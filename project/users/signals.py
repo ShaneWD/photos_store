fd6f26b94d781e9768d940d3d613236240e1c197
Shane <>
2021-02-10 07:43:38 -0500

    when account is created, profile is also created

---
+++ project/users/signals.py
@@ -0,0 +1,16 @@
+from django.db.models.signals import post_save
+from .models import User # user is the sender
+from django.dispatch import receiver # is the reciever 
+from .models import Profile
+
+
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_profile(sender, instance, created, **kwargs):
+    instance.profile.save()
+    # instance is the user
+
