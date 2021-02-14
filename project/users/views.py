c6d28db7c3047e47deb45d921c82431966f500ca
Shane <>
2021-02-13 20:36:52 -0500

    pre-populates user profile informtaion

--- project/users/views.py
+++ project/users/views.py
@@ -1,14 +1,17 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .forms import UserUpdateForm, ProfileUpdateForm
+
 # Create your views here.
 
 def home(request):
     return HttpResponse("<h1>Hello</h1>")
 
+
+
 def profile(request):
-    u_form = UserUpdateForm()
-    p_form = ProfileUpdateForm()
+    u_form = UserUpdateForm(instance=request.user)
+    p_form = ProfileUpdateForm(instance=request.user.profile)
 
     context = {
         'u_form':u_form,
