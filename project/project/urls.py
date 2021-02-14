9db57082ec406d76711791bfbc61ec81cd6b16ee
Shane <>
2021-02-13 19:35:30 -0500

    set up profile page

--- project/project/urls.py
+++ project/project/urls.py
@@ -22,6 +22,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include("main.urls")),
+    path('user/', include("users.urls")),
     
 ]
 
