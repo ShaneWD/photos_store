8c5da94cbd629e7971d1f17aeed1778213388867
Shane <>
2021-02-14 13:49:25 -0500

    created model for pictures

--- project/main/models.py
+++ project/main/models.py
@@ -1,3 +1,9 @@
 from django.db import models
-
+from users.models import User
 # Create your models here.
+class Pictures(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to="pictures")
+    title = models.CharField(max_length=50, default='')
+    description = models.CharField(max_length=200, default='')
+    tags = models.CharField(max_length=100, default='')
