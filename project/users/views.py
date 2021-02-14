c52365bee9f13fd56eb7d0ec1ba171fc9385a8dd
Shane <>
2021-02-13 20:48:18 -0500

    saves profile changes

--- project/users/views.py
+++ project/users/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .forms import UserUpdateForm, ProfileUpdateForm
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 # Create your views here.
 
@@ -8,10 +10,21 @@ def home(request):
     return HttpResponse("<h1>Hello</h1>")
 
 
-
+@login_required
 def profile(request):
-    u_form = UserUpdateForm(instance=request.user)
-    p_form = ProfileUpdateForm(instance=request.user.profile)
+    if request.method == "POST":
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateForm(request.POST, request.FILES,instance=request.user.profile)
+        if u_form.is_valid and p_form.is_valid:
+            u_form.save() 
+            p_form.save()
+            messages.success(request, "Your account has been updated!")
+            return redirect('profile')
+
+    else:
+        u_form = UserUpdateForm(instance=request.user)
+        p_form = ProfileUpdateForm(instance=request.user.profile)
+
 
     context = {
         'u_form':u_form,
