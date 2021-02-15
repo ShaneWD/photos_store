5f7c1b9c7ef72cef9bfa4e4bc4d8d111b0ed8b6b
Shane <>
2021-02-14 19:26:13 -0500

    displays each picture of admin in homepage

--- project/main/views.py
+++ project/main/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from .models import Pictures
 # Create your views here.
 
 
 @login_required
 def home(request):
     context = {
-
+        "pictures": Pictures.objects.all()
     }
     
     return render(request, "main/home.html", context)
