24c04bfc5c7c47739d0be4ee940a297032e2d724
Shane <>
2021-02-17 09:36:54 -0500

    implemented pagination. Three objects per page

--- project/main/views.py
+++ project/main/views.py
@@ -2,13 +2,19 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
 from .models import Pictures
+from django.core.paginator import Paginator
 # Create your views here.
 
 
 @login_required
 def home(request):
+    pictures = Pictures.objects.all()
+    paginator = Paginator(pictures, 3)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
     context = {
-        "pictures": Pictures.objects.all()
+        "page_obj": page_obj,
+        # "pictures": pictures
     }
     
     return render(request, "main/home.html", context)
